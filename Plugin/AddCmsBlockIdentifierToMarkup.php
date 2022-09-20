e2f64407daa01ddd78eebd94dc69e2525cb47c04
pykettk <pykettk@gmail.com>
2022-09-20 17:46:19 +0100

    Add CMS Block Identifer to Markup

---
+++ Plugin/AddCmsBlockIdentifierToMarkup.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright © element119. All rights reserved.
+ * See LICENCE.txt for licence details.
+ */
+declare(strict_types=1);
+
+namespace Element119\CmsIdentifierMarkup\Plugin;
+
+use Element119\CmsIdentifierMarkup\Scope\Config;
+use Magento\Cms\Api\BlockRepositoryInterface;
+use Magento\Cms\Api\Data\BlockInterface;
+
+class AddCmsBlockIdentifierToMarkup
+{
+    /** @var Config */
+    private Config $moduleConfig;
+
+    /**
+     * @param Config $moduleConfig
+     */
+    public function __construct(
+        Config $moduleConfig
+    ) {
+        $this->moduleConfig = $moduleConfig;
+    }
+
+    /**
+     * Add HTML data-block-identifier attribute to wrapping element of CMS blocks.
+     *
+     * @param BlockRepositoryInterface $subject
+     * @param BlockInterface $block
+     * @return BlockInterface[]
+     */
+    public function beforeSave(
+        BlockRepositoryInterface $subject,
+        BlockInterface $block
+    ): array {
+        if ($this->moduleConfig->isBlockIdentifierMarkupEnabled() && $block->getIdentifier()) {
+            $block->setContent(
+                substr_replace(
+                    $block->getContent(),
+                    " data-{$this->moduleConfig->getBlockIdentifierDataAttributeName()}=\"{$block->getIdentifier()}\"",
+                    strpos($block->getContent(), ' '),
+                    0
+                )
+            );
+        }
+
+        return [$block];
+    }
+}
