b11da64007e7cecb912c9027aecc12c18e3e754f
pykettk <pykettk@gmail.com>
2022-09-20 17:46:01 +0100

    Add Module Configuration Options

---
+++ Scope/Config.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © element119. All rights reserved.
+ * See LICENCE.txt for licence details.
+ */
+declare(strict_types=1);
+
+namespace Element119\CmsIdentifierMarkup\Scope;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class Config
+{
+    const XML_PATH_CMS_IDENTIFIER_MARKUP_ENABLED = 'cms/pagebuilder/block_identifier_markup_enabled';
+    const XML_PATH_CMS_IDENTIFIER_DATA_ATTR_NAME = 'cms/pagebuilder/block_identifier_attribute_name';
+
+    /** @var ScopeConfigInterface */
+    private ScopeConfigInterface $scopeConfig;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @param int|string|null $scopeCode
+     * @return bool
+     */
+    public function isBlockIdentifierMarkupEnabled($scopeCode = null): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_CMS_IDENTIFIER_MARKUP_ENABLED,
+            ScopeInterface::SCOPE_STORE,
+            $scopeCode
+        );
+    }
+
+    /**
+     * @param $scopeCode
+     * @return string
+     */
+    public function getBlockIdentifierDataAttributeName($scopeCode = null): string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_CMS_IDENTIFIER_DATA_ATTR_NAME,
+            ScopeInterface::SCOPE_STORE,
+            $scopeCode
+        );
+    }
+}
